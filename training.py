b859fee7c0a28be321b722ac5782a718b95c0ab7
Hayo1706 <hayoriem@gmail.com>
2024-10-11 23:30:17 +0200

    Added statistics to training.py

--- training.py
+++ training.py
@@ -1,3 +1,4 @@
+import math
 import pickle
 import random
 import neat
@@ -5,6 +6,7 @@ import os
 import numpy as np
 import multiprocessing as mp
 import matplotlib.pyplot as plt
+
 
 class Strategy:
     def __init__(self):
@@ -27,6 +29,21 @@ class AlwaysDefect(Strategy):
         return "D"
 
 
+class CooperateUntilThreeDefections(Strategy):
+    def make_move(self, ownHistory, oppHistory):
+        # Cooperate if no previous history
+        if len(oppHistory) < 3:
+            return "C"
+
+        # Check if the opponent has defected three times in a row at any point
+        for i in range(len(oppHistory) - 2):
+            if oppHistory[i:i + 3] == ["D", "D", "D"]:
+                return "D"
+
+        # Otherwise, continue cooperating
+        return "C"
+
+
 class Genome(Strategy):
     def __init__(self, genome, config):
         super().__init__()
@@ -37,7 +54,7 @@ class Genome(Strategy):
     def make_move(self, ownHistory, oppHistory):
         input = [0] * 40
 
-        for idx, (opp,own) in enumerate(zip(oppHistory, ownHistory)):
+        for idx, (opp, own) in enumerate(zip(oppHistory, ownHistory)):
             if own == "C":
                 input[idx * 2] = 1
             else:
@@ -50,7 +67,6 @@ class Genome(Strategy):
 
         move = self.net.activate(input)
         return "C" if move[0] < 0.5 else "D"
-
 
 class GameStats:
     def __init__(self):
@@ -65,8 +81,13 @@ class GameStats:
         self.amount_start_defect = 0
         self.amount_start_cooperate = 0
 
+        self.amount_forgiveness = 0
+
         self.history1 = []
         self.history2 = []
+
+        self.complexity1 = 0
+        self.complexity2 = 0
 
 
 # Define the reward system for the Prisoner's Dilemma
@@ -111,6 +132,13 @@ def play_game(genome1: Strategy, genome2: Strategy, num_rounds=20):
         else:
             gamestats.amount_one_cooperate += 1
 
+        if not len(play1_history) == 0 and play1_history[-1] == "D" and play2_history[-1] == "D":
+            # calculate forgiveness, can only be done if there is a previous round
+            if move1 == "C":
+                gamestats.amount_forgiveness += 1
+            if move2 == "C":
+                gamestats.amount_forgiveness += 1
+
         play1_history.append(move1)
         play2_history.append(move2)
 
@@ -121,6 +149,11 @@ def play_game(genome1: Strategy, genome2: Strategy, num_rounds=20):
 
         gamestats.history1 = play1_history
         gamestats.history2 = play2_history
+
+        # measure genetic complexity
+        gamestats.complexity1 = genome1.genome.size()
+        if isinstance(genome2, Genome):
+            gamestats.complexity2 = genome2.genome.size()
 
     return score1, score2, gamestats
 
@@ -139,13 +172,12 @@ def evaluate_genomes(genomes, config):
         genome.amount_of_games = 0
 
     # Create a list of all genome pairs to be evaluated
-    genome_pairs = create_genome_pairs(genomes)
+    genome_pairs = create_genome_pairs_predefined(genomes)
     results = []
     with mp.Pool() as pool:
         results = pool.starmap(play_game, genome_pairs)
     # for genome1, genome2 in genome_pairs:
     #     results.append(play_game(genome1, genome2))
-
 
     # Update the fitness of each genome based on the results
     for (genome1, genome2), (score1, score2, gamestats) in zip(genome_pairs, results):
@@ -169,7 +201,7 @@ def evaluate_genomes(genomes, config):
 
 
 def create_genome_pairs_random(genomes, subset_size):
-    # Create a list of all genome pairs to be evaluated
+    # Create a list of all genome pairs randomly, with a subset of the population
     genome_pairs = []
     for (genome_id1, genome1) in genomes:
         opponents = random.sample(genomes, subset_size)
@@ -179,50 +211,85 @@ def create_genome_pairs_random(genomes, subset_size):
             ))
     return genome_pairs
 
+
 def create_genome_pairs(genomes):
     # Create a list of all genome pairs to be evaluated
     genome_pairs = []
     for idx, (genome_id1, genome1) in enumerate(genomes):
         # opponents = random.sample(genomes, subset_size)
-        for genome_id2, genome2 in genomes[idx+1:]:
+        for genome_id2, genome2 in genomes[idx + 1:]:
             genome_pairs.append((
                 Genome(genome1, config), Genome(genome2, config)
             ))
     return genome_pairs
 
 
-def create_genome_pairs_tft(genomes, subset_size):
+def create_genome_pairs_predefined(genomes):
     genome_pairs = []
-    for genome_id1, genome1 in genomes:
-        genome_pairs.append((
-            Genome(genome1, config), TitForTat()))
-        genome_pairs.append((
-            Genome(genome1, config), AlwaysDefect()))
-        genome1.amount_of_games += 2
+    for idx, (genome_id1, genome1) in enumerate(genomes):
+        # opponents = random.sample(genomes, subset_size)
+        for genome_id2, genome2 in genomes[idx + 1:]:
+            genome_pairs.append((
+                Genome(genome1, config), Genome(genome2, config)
+            ))
+        # for i in range(30):
+        #     genome_pairs.append((
+        #         Genome(genome1, config), CooperateUntilThreeDefections()))
+        for i in range(30):
+            genome_pairs.append((
+                Genome(genome1, config), AlwaysDefect()))
 
     return genome_pairs
 
 
-def calculate_percentage_both_cooperate(gamestats_per_generation):
-    percentages = {}
+def calculate_percentage_stats_per_five_generations(gamestats_per_generation):
+    average_cooperate = {}
+    average_first_cooperate = {}
+    average_complexity = {}
+    average_forgiveness = {}
 
-    for generation, stats_list in gamestats_per_generation.items():
-        total_both_cooperate = sum(stat.amount_both_cooperate for stat in stats_list)
-        total_both_defect = sum(stat.amount_both_defect for stat in stats_list)
-        total_one_cooperate = sum(stat.amount_one_cooperate for stat in stats_list)
+    # Calculate for every 5 rounds
+    for i in range(math.floor(len(gamestats_per_generation.items()) / 5)):
+        total_both_cooperate = 0
+        total_both_defect = 0
+        total_one_cooperate = 0
 
-        # Calculate total moves
+        total_start_cooperate = 0
+        total_start_defect = 0
+
+        total_complexity = 0
+        total_genomes = 0
+
+        total_forgiveness = 0
+        total_games = 0
+
+        for stats_list in list(gamestats_per_generation.values())[i:i + 5]:
+            total_both_cooperate += sum(stat.amount_both_cooperate for stat in stats_list)
+            total_both_defect += sum(stat.amount_both_defect for stat in stats_list)
+            total_one_cooperate += sum(stat.amount_one_cooperate for stat in stats_list)
+
+            total_start_cooperate += sum(stat.amount_start_cooperate for stat in stats_list)
+            total_start_defect += sum(stat.amount_start_defect for stat in stats_list)
+
+            total_forgiveness += sum(stat.amount_forgiveness for stat in stats_list)
+            total_games += len(stats_list)
+
+            for stat in stats_list:
+                total_complexity += stat.complexity1[0] * stat.complexity1[1]
+                total_genomes += 1
+                if stat.complexity2 != 0:
+                    total_complexity += stat.complexity2[0] * stat.complexity2[1]
+                    total_genomes += 1
+
         total_moves = total_both_cooperate + total_both_defect + total_one_cooperate
+        total_first_moves = total_start_cooperate + total_start_defect
 
-        # Avoid division by zero
-        if total_moves > 0:
-            percentage = (total_both_cooperate / total_moves) * 100
-        else:
-            percentage = 0
+        average_forgiveness[i * 5] = (total_forgiveness / total_games)
+        average_cooperate[i * 5] = (total_both_cooperate / total_moves) * 100
+        average_first_cooperate[i * 5] = (total_start_cooperate / total_first_moves) * 100
+        average_complexity[i * 5] = total_complexity / total_genomes
 
-        percentages[generation] = percentage
-
-    return percentages
+    return average_cooperate, average_first_cooperate, average_complexity, average_forgiveness
 
 
 def plot_percentage_both_cooperate(percentages):
@@ -240,27 +307,6 @@ def plot_percentage_both_cooperate(percentages):
     plt.show()
 
 
-def calculate_percentage_niceness(gamestats_per_generation):
-    percentages = {}
-
-    for generation, stats_list in gamestats_per_generation.items():
-        total_start_cooperate = sum(stat.amount_start_cooperate for stat in stats_list)
-        total_start_defect = sum(stat.amount_start_defect for stat in stats_list)
-
-        # Calculate total first moves
-        total_first_moves = total_start_cooperate + total_start_defect
-
-        # Avoid division by zero
-        if total_first_moves > 0:
-            percentage = (total_start_cooperate / total_first_moves) * 100
-        else:
-            percentage = 0
-
-        percentages[generation] = percentage
-
-    return percentages
-
-
 def plot_percentage_niceness(percentages):
     generations = list(percentages.keys())
     percentage_values = list(percentages.values())
@@ -275,8 +321,65 @@ def plot_percentage_niceness(percentages):
     plt.grid()
     plt.show()
 
-# Calculate percentages and plot
+def plot_complexity(percentage):
+    generations = list(percentage.keys())
+    percentage_values = list(percentage.values())
 
+    plt.figure(figsize=(10, 5))
+    plt.plot(generations, percentage_values, marker='o', linestyle='-', color='r')
+    plt.title('Average Complexity per Generation')
+    plt.xlabel('Generation')
+    plt.ylabel('Average Complexity')
+    plt.xticks(generations)
+    plt.grid()
+    plt.show()
+
+
+def plot_combined(percentages_both_cooperate, percentages_niceness, complexities, average_forgiveness):
+    generations = list(percentages_both_cooperate.keys())
+
+    # Create the first y-axis
+    fig, ax1 = plt.subplots(figsize=(10, 5))
+
+    # Plot percentage of both cooperate
+    ax1.plot(generations, list(percentages_both_cooperate.values()), marker='o', linestyle='-', color='b',
+             label='Both Cooperate (%)')
+    ax1.set_title('Strategies evolution per Generation')
+    ax1.set_xlabel('Generation')
+    ax1.set_ylabel('Percentage of Both Cooperate (%)', color='b')
+    ax1.set_ylim(0, 100)  # Set y-axis limits from 0 to 100
+    ax1.tick_params(axis='y', labelcolor='b')
+
+    # Create a second y-axis for percentage of niceness
+    ax2 = ax1.twinx()
+    ax2.plot(generations, list(percentages_niceness.values()), marker='o', linestyle='-', color='g',
+             label='First Move Cooperation (%)')
+    ax2.set_ylabel('Percentage of First Move Cooperation (%)', color='g')
+    ax2.set_ylim(0, 100)  # Set y-axis limits from 0 to 100
+    ax2.tick_params(axis='y', labelcolor='g')
+
+    # Create a third y-axis for average complexity
+    ax3 = ax1.twinx()
+    ax3.spines['right'].set_position(('outward', 60))  # Move the third y-axis outward
+    ax3.plot(generations, list(complexities.values()), marker='o', linestyle='-', color='r', label='Average Complexity')
+    ax3.set_ylabel('Average Complexity', color='r')
+    ax3.tick_params(axis='y', labelcolor='r')
+
+    # Create a fourth y-axis for average forgiveness
+    ax4 = ax1.twinx()
+    ax4.spines['right'].set_position(('outward', 120))  # Move the fourth y-axis further outward
+    ax4.plot(generations, list(average_forgiveness.values()), marker='o', linestyle='-', color='y', label='Average Forgiveness per game')
+    ax4.set_ylabel('Average Forgiveness per game', color='y')
+    ax4.tick_params(axis='y', labelcolor='y')
+
+    # Set x-ticks
+    ax1.set_xticks([gen for gen in generations if gen % 100 == 0])
+
+    # Adding a grid
+    ax1.grid()
+
+    # Show the plot
+    plt.show()
 
 if __name__ == '__main__':
     # Load the NEAT config file
@@ -293,11 +396,14 @@ if __name__ == '__main__':
 
     winner = population.run(evaluate_genomes, 1000)
 
-    percentages = calculate_percentage_both_cooperate(gamestats_per_generation)
-    plot_percentage_both_cooperate(percentages)
+    average_cooperate, average_first_cooperate, average_complexity, average_forgiveness = calculate_percentage_stats_per_five_generations(
+        gamestats_per_generation)
 
-    percentages = calculate_percentage_niceness(gamestats_per_generation)
-    plot_percentage_niceness(percentages)
+    plot_combined(average_cooperate, average_first_cooperate, average_complexity, average_forgiveness)
+
+    # plot_percentage_niceness(average_first_cooperate)
+    # plot_percentage_both_cooperate(average_cooperate)
+    # plot_complexity(average_complexity)
 
     with open("WinnerGenome", 'wb') as f:
         # Save the genome and neural network state
